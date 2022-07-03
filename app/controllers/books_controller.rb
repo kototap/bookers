01121877fb3f782eff3b63421c3d6debc90ff360
EC2 Default User <ec2-user@ip-172-31-6-141.ap-northeast-1.compute.internal>
2022-07-03 03:07:25 +0000

    [Update]3-12

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+  end
+  
+  def new
+    @book = Book.new
+    
+  end
+  
+  def create
+  end
+
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+ resources :books
+  get root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# books GET    /books(.:format)     books#index
+#       POST   /books(.:format)     books#create
+
+# new_book GET    /books/new(.:format)      books#new
+# edit_book GET    /books/:id/edit(.:format)     books#edit
+# book    GET    /books/:id(.:format)         books#show
+#.        PATCH  /books/:id(.:format)     books#update
+#         PUT    /books/:id(.:format)   books#update
+#         DELETE /books/:id(.:format)     books#destroy
+# root GET    /       homes#top
